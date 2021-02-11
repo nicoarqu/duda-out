e376f443da55c974d8f44b4be2b07c5531bfa620
Nico Araya <nlaraya@uc.cl>
2021-02-11 20:49:13 -0300

    ✨ programs  navigation screens

--- src/api/programs/constants.js
+++ src/api/programs/constants.js
@@ -1,14 +1,17 @@
 export const programs = [
   {
+    id: 1,
     title: "Tutorías y orientación individual",
     items: [
       {
+        id: 1,
         title: "Programa de fortalecimiento para la escritura y oratoria",
         rating: 5.0,
         desc: `Si existen complicaciones con algún trabajo escrito, este programa te guía a través de sesiones de 1 hora de 2 a 4 veces por semestre.
             Se permite agendar horas dependiendo de los horarios disponibles de los tutores, sólo debes llenar un formulario de inscripción!!`,
       },
       {
+        id: 2,
         title: "Tutorías de matemáticas",
         rating: 5.0,
         desc: `Sesiones guiadas con ejercicios propuestos por ayudantes de duración 1 hora, para los siguientes ramos:
@@ -21,11 +24,13 @@ export const programs = [
             - Cálculo II- Comercial`,
       },
       {
+        id: 3,
         title: "Talleres de Escritura y Lectura Universitaria por carrera",
         rating: 5.0,
         desc: "Derecho, Ingeniería Comercial, Psicología, Ingeniería Civil",
       },
       {
+        id: 4,
         title: "Tardes de estudio",
         rating: 5.0,
         desc: `Sesiones en donde los alumnos realizan preguntas a los ayudantes
@@ -34,14 +39,17 @@ export const programs = [
     ],
   },
   {
+    id: 2,
     title: "Artes Liberales",
     items: [
       {
+        id: 1,
         title: "Centro de escritura y oratoria",
         rating: 5.0,
         desc: `Brinda material educativo de escritura académica, lenguaje paraverbal y argumentación.`,
       },
       {
+        id: 2,
         title: "Escritura",
         rating: 5.0,
         desc: `Supervisión y guía en producción de textos, citas y bibliografía, corrección de textos y taller de ortografía en línea.`,
