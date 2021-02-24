d4058d882e7b9b757ec5b87140201bdb802bafa8
Nico Araya <nlaraya@uc.cl>
2021-02-24 12:08:13 -0300

    🍱 add google fonts roboto - bitter

--- App.js
+++ App.js
@@ -3,17 +3,40 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { Provider } from "react-redux";
 import { LogBox } from "react-native";
+import {
+  useFonts,
+  Roboto_500Medium as RobotoMedium,
+  Roboto_400Regular as RobotoRegular,
+  Roboto_300Light as RobotoLight,
+} from "@expo-google-fonts/roboto";
+import {
+  Bitter_400Regular as BitterRegular,
+  Bitter_700Bold as BitterBold,
+} from "@expo-google-fonts/bitter";
+import { ActivityIndicator } from "react-native-paper";
 import store from "./src/redux/store";
 import MainStack from "./src/navigation";
 
 LogBox.ignoreAllLogs(true);
 
-const App = () => (
-  <NavigationContainer>
-    <Provider store={store}>
-      <MainStack />
-    </Provider>
-  </NavigationContainer>
-);
+const App = () => {
+  const [fontsLoaded] = useFonts({
+    RobotoMedium,
+    RobotoRegular,
+    RobotoLight,
+    BitterBold,
+    BitterRegular,
+  });
+  if (!fontsLoaded) {
+    return <ActivityIndicator />;
+  }
+  return (
+    <NavigationContainer>
+      <Provider store={store}>
+        <MainStack />
+      </Provider>
+    </NavigationContainer>
+  );
+};
 
 export default App;
